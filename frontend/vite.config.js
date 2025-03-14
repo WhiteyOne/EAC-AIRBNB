a86cd947c08f3a9ef11be410d53db5951c0bfc2b
WhiteyOne <Cayden.Hanks.Pro@gmail.com>
2025-03-13 20:58:21 -0700

    fixed mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(( mode ) => ({
   plugins: [
     react(),
     eslint({
       lintOnStart: true,
-      failOnError: mode !== "production"
+      failOnError: mode === "production"
     })
   ],
   server: {
